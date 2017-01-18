5f374fc421e3c4972f6c476a8ef61a414fc74129
petrkruppa <petrkruppa@gmail.com>
2017-01-18 18:03:30 +0300

    update

--- public/chat.js
+++ public/chat.js
@@ -1,50 +1,56 @@
 $(function() {
-	
+ 
     $("#chatControls").hide();
+ $('#collapsible').slideToggle('slow');
     $("#pseudoSet").click(function() {setPseudo()});
-   	$("#messageInput" ).keyup(function() {TypingDetected();});   
-	$("#pseudoInput").on('keyup', function (e) {
-		if (e.keyCode == 13) { setPseudo();  }
-	});
-		// This demo depends on the canvas element
-	if(!('getContext' in document.createElement('canvas'))){
-		alert('Sorry, it looks like your browser does not support canvas!');
-		return false;
-	}
+ $("#clearbutton").click(function(){
+ paper.project.activeLayer.removeChildren();
+    paper.view.draw(); 
+});
+ 
+    $("#messageInput" ).keyup(function() {TypingDetected();});   
+ $("#pseudoInput").on('keyup', function (e) {
+  if (e.keyCode == 13) { setPseudo();  }
+ });
+  // This demo depends on the canvas element
+ if(!('getContext' in document.createElement('canvas'))){
+  alert('Sorry, it looks like your browser does not support canvas!');
+  return false;
+ }
 
 });
 var socket = io.connect();
 var sessionId = socket.io.engine.id;
 //alert(sessionId);
 
+
+
 socket.on('message', function(data) {
     addMessage(data['message'], data['pseudo']);
-	
+ 
 });
-
-
 
 socket.on('users', function(data) {
     let html = "Гостей в чате: " +(data.length+1)+". ";
-	data.forEach(function(value, index){
-		if (value.sessid != sessionId) html+="<span id="+ value.sessid+">"+value.name+"</span>,";
-		
-	 });
-	html+="<span id="+ sessionId +">YOU</span>";
-	$("#whoisonline").html(html);
-	console.log(data);// addMessage(data['message'], data['pseudo']);
+ data.forEach(function(value, index){
+  if (value.sessid != sessionId) html+="<span id="+ value.sessid+">"+value.name+"</span>,";
+  
+  });
+ html+="<span id="+ sessionId +">YOU</span>";
+ $("#whoisonline").html(html);
+ console.log(data);// addMessage(data['message'], data['pseudo']);
 });
 
 socket.on('usersonconnect', function(data) {
     //let html = "Гостей в чате: " +data.length+". ";
-	$("#alreadythere").html("Уже в чате: "+data);
-	//console.log(data);// addMessage(data['message'], data['pseudo']);
+ $("#alreadythere").html("Уже в чате: "+data);
+ //console.log(data);// addMessage(data['message'], data['pseudo']);
 });
 
 socket.on('connect', function() {
-	console.log('connected');
-	//$("#chatEntries").append('<h3>Добро пожаловать в чат, пожалуйста, представтесь!</h3>');
-	sessionId = socket.io.engine.id;
+ console.log('connected');
+ //$("#chatEntries").append('<h3>Добро пожаловать в чат, пожалуйста, представтесь!</h3>');
+ sessionId = socket.io.engine.id;
    
 
 });
@@ -73,12 +79,8 @@ function setPseudo() {
 }
 
 function TypingDetected(){
-	
-	socket.emit('typing',$('#messageInput').val());
-	$("#myTexting").html('<div class="message"><p>Я: ' + $("#messageInput" ).val() + '</p></div>');
-	
+ 
+ socket.emit('typing',$('#messageInput').val());
+ $("#myTexting").html('<div class="message"><p>Я: ' + $("#messageInput" ).val() + '</p></div>');
+ 
 }
-
-
-
-
